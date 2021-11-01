a4b03b67d9ea577a6fce02c9be87d1eba86dd664
SHIV SHAKTI <33700793+shikari1893@users.noreply.github.com>
2021-11-01 10:31:41 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,20 +1,11 @@
-#!/bin/sh 
-set -e
+#!/bin/sh
+
+echo "My name is ${fyrelog} "
+
+
 
 shiv(){
    cat shakti.txt
-   echo $fyrelog
- #  while getopts u:a:f: flag
-#do
-#    case "${flag}" in
-#        u) username=${OPTARG};;
-#        a) age=${OPTARG};;
- #       f) fullname=${OPTARG};;
-#    esac
-#done
-#echo "Username: $username";
-#echo "Age: $age";
-#echo "Full Name: $fullname";
    
 }
 
