c6a378c9af6de744952581281ae004c8b074b581
SHIV SHAKTI <33700793+shikari1893@users.noreply.github.com>
2021-11-01 09:21:50 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,7 +3,18 @@ set -e
 
 shiv(){
    cat shakti.txt
-   echo $fyrelog
+   while getopts u:a:f: flag
+do
+    case "${flag}" in
+        u) username=${OPTARG};;
+        a) age=${OPTARG};;
+        f) fullname=${OPTARG};;
+    esac
+done
+echo "Username: $username";
+echo "Age: $age";
+echo "Full Name: $fullname";
+   #echo $fyrelog
 }
 
 shiv
