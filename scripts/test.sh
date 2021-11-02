215c9b6e8c0ff736398fde14143bf745a66dd201
SHIV SHAKTI <33700793+shikari1893@users.noreply.github.com>
2021-11-02 17:32:16 +0530

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,58 +1,9 @@
 #!/bin/sh
 
 
-#setLocalVariables()
-# {
-#  nodeNumber=`1`
-#  resourceType=`VM`
-#  export nodeNumber resourceType
-#}
-
-#echo $resourceType
-
-#set nodeNumber = $4
-#set resourceType = $5
-
-
-#EVAL SCRIPT
-
-#nodeNumber=1
-
-#$s="test.sh "${nodeNumber} 
-#eval $s
-#echo $s
-
-#v="${nodeNumber}"
-#o=$s $v
-#echo $o
-#eval $s
-
-
-#cmd="pwd ${nodeNumber}"
-#echo "$cmd"
-
-
-
-
-#resourceType=VM
-#nodeNumber= 1
-
-
 echo "The script name : $0"
 echo "The value of the first argument to the script : $1"
 echo "The value of the first argument to the script : $2"
 echo "The value of the first argument to the script : $3"
-echo "The value of the first argument to the script : $4
+echo "The value of the first argument to the script : $4"
 echo "The value of the first argument to the script : $5"
-
-
-
-#shiv(){
- #  cat shakti.txt
-   
-  # }
-   
-
-
-#setLocalVariables
-#shiv
