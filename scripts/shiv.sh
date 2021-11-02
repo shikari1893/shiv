6eb515a0faca852d3fe81516d4bc43d526d401d1
SHIV SHAKTI <33700793+shikari1893@users.noreply.github.com>
2021-11-02 16:51:18 +0530

    Update shiv.sh

--- scripts/shiv.sh
+++ scripts/shiv.sh
@@ -5,7 +5,7 @@
               nodeNumber=1
               resourceType=VM
               chmod -R 755 scripts
-              test.sh ${FYRE_ID} ${FYRE_TOKEN} ${BUILD_DISPLAY_NAME} ${resourceType} ${nodeNumber}
+              scripts/test.sh ${FYRE_ID} ${FYRE_TOKEN} ${BUILD_DISPLAY_NAME} ${resourceType} ${nodeNumber}
               fyreStat=$?
 	      if [[ ${fyreStat} = 1 ]] ; then exit 1 ; fi
             
