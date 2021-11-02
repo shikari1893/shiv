c92f64339d966578cf87bd566f96b1968a9611be
SHIV SHAKTI <33700793+shikari1893@users.noreply.github.com>
2021-11-02 12:09:52 +0530

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
 
-echo "NM is ${nodeNumber}"
+echo "NM is $1"
 
 shiv(){
    cat shakti.txt
    
    }
 
-if [ "$1" != "" ]; then
-    echo "Positional parameter 1 contains value 1"
-else
-    echo "Positional parameter 1 is empty"
-fi
 nodeNumber=${1}
 shiv
