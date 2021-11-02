9de88b91c52cf152a698457b931557bd40c54379
SHIV SHAKTI <33700793+shikari1893@users.noreply.github.com>
2021-11-02 12:52:36 +0530

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -8,9 +8,9 @@ setLocalVariables()
   export nodeNumber resourceType
 }
 
-echo "NM is ${nodeNumber} "
-set $nodeNumber = $4
-set $resourceType = $5
+echo "NM is $nodeNumber "
+set nodeNumber = $4
+set resourceType = $5
 echo "The script name : $0"
 echo "The value of the first argument to the script : $1"
 echo "The value of the first argument to the script : $2"
