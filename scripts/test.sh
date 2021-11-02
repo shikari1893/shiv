e43748e0535edfb21cf961b5bd3b267fd3f9ba48
SHIV SHAKTI <33700793+shikari1893@users.noreply.github.com>
2021-11-02 12:43:55 +0530

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,12 +3,16 @@
 nodeNumber=1
 resourceType=VM
 set $nodeNumber = $4
-set $resourceType = $4
+set $resourceType = $5
 echo "The script name : $0"
 echo "The value of the first argument to the script : $1"
+echo "The value of the first argument to the script : $2"
+echo "The value of the first argument to the script : $3"
+echo "The value of the first argument to the script : $4"
+echo "The value of the first argument to the script : $5"
 echo "$# arguments"
 echo "$* list of arguments"
-echo " resourceType is : $6 "
+echo "resourceType is : $6"
 
 shiv(){
    cat shakti.txt
