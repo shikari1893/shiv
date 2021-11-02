776af0476b8ab9a8ae2e17eedbe4fb50937e679e
SHIV SHAKTI <33700793+shikari1893@users.noreply.github.com>
2021-11-02 16:55:39 +0530

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -40,12 +40,12 @@
 
 echo "The script name : $0"
 echo "The value of the first argument to the script : $1"
-#echo "The value of the first argument to the script : $2"
-#echo "The value of the first argument to the script : $3"
-#echo "The value of the first argument to the script : $4
-#echo "The value of the first argument to the script : $5"
-#echo "$# arguments"
-#echo "$* list of arguments"
+echo "The value of the first argument to the script : $2"
+echo "The value of the first argument to the script : $3"
+echo "The value of the first argument to the script : $4
+echo "The value of the first argument to the script : $5"
+echo "$# arguments"
+echo "$* list of arguments"
 
 
 #shiv(){
