cf9f391fe10e4883686e796b5226f9f67494bacb
SHIV SHAKTI <33700793+shikari1893@users.noreply.github.com>
2021-11-02 12:42:15 +0530

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,8 +2,8 @@
 
 nodeNumber=1
 resourceType=VM
-set $nodeNumber = $5
-set $resourceType = $6
+set $nodeNumber = $4
+set $resourceType = $4
 echo "The script name : $0"
 echo "The value of the first argument to the script : $1"
 echo "$# arguments"
