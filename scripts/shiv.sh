c11206dc600475100535fa193ea3e3242b7d4b69
SHIV SHAKTI <33700793+shikari1893@users.noreply.github.com>
2021-11-02 16:41:42 +0530

    Update shiv.sh

--- scripts/shiv.sh
+++ scripts/shiv.sh
@@ -1,4 +1,11 @@
 #!bin/sh
 
-echo "The value of the first argument to the script : $1"
 
+              fyrelog=${fyrePath}/fyrerequest.log
+              nodeNumber=1
+              resourceType=VM
+              chmod -R 755 scripts
+              scripts/provision-fyre.sh ${FYRE_ID} ${FYRE_TOKEN} ${BUILD_DISPLAY_NAME} ${resourceType} ${nodeNumber}
+              fyreStat=$?
+	            if [[ ${fyreStat} = 1 ]] ; then exit 1 ; fi
+            
