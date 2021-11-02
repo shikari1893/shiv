d90fd4ccd88c2793d85facd5db45b278c2a71336
SHIV SHAKTI <33700793+shikari1893@users.noreply.github.com>
2021-11-02 13:40:15 +0530

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -12,6 +12,13 @@
 
 #set nodeNumber = $4
 #set resourceType = $5
+
+cmd = "echo resourceType{VM},nodeNumber{1}"
+$cmd
+#resourceType=VM
+#nodeNumber= 1
+
+
 echo "The script name : $0"
 echo "The value of the first argument to the script : $1"
 echo "The value of the first argument to the script : $2"
