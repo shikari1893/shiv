055556e960027778bad222a2cc2e9023735ebebc
SHIV SHAKTI <33700793+shikari1893@users.noreply.github.com>
2021-11-02 12:29:26 +0530

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,6 +5,7 @@ set $nodeNumber = $1
 echo "The script name : $0"
 echo "The value of the first argument to the script : $1"
 echo "$# arguments"
+echo "$* list of arguments"
 
 shiv(){
    cat shakti.txt
