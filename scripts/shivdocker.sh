e3fcd0cef6ba17a2960cb41e7e560bf9409baeea
SHIV SHAKTI <33700793+shikari1893@users.noreply.github.com>
2021-11-03 13:57:24 +0530

    Update shivdocker.sh

--- scripts/shivdocker.sh
+++ scripts/shivdocker.sh
@@ -6,5 +6,7 @@
               wkcNode="$lwkcBuildName-b${BUILD_NUMBER}-host-1.fyre.ibm.com"
               jenkinsHost="https://dataconn-jenkins.swg-devops.com"
               echo "Working on ${wkcNode}"
-              sshpass -p ${FYRENODE_PW} scp -o StrictHostKeyChecking=no scripts/runSwarm.sh root@${wkcNode}:/tmp
-              sshpass -p ${FYRENODE_PW} ssh -o StrictHostKeyChecking=no root@${wkcNode} "chmod 755 /tmp/runSwarm.sh; /tmp/runSwarm.sh ${JENK_PASS} ${jenkinsHost} ${wkcNodeLabel} ${JENK_ID}"
+              scripts/docker.sh root@${wkcNode}:/tmp
+              #sshpass -p ${FYRENODE_PW} ssh -o StrictHostKeyChecking=no root@${wkcNode} "chmod 755 /tmp/runSwarm.sh; /tmp/runSwarm.sh ${JENK_PASS} ${jenkinsHost} ${wkcNodeLabel} ${JENK_ID}"
+
+#sshpass -p ${FYRENODE_PW} scp -o StrictHostKeyChecking=no
